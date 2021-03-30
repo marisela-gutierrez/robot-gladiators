66ba7dd817728293bb2a921360daf72d1fd153dd
marigut <mari.gutierrez96@yahoo.com>
2021-03-29 20:39:59 -0500

    playername

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+//fight(); 
+
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation marks around playerName
+console.log(playerName);
+
